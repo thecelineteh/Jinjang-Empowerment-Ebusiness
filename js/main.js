6442202d421537c7105136ef222cff3d60fda516
AdrianFoo97 <foo_jun_wei@hotmail.com>
2018-03-12 18:08:40 +0800

    Content modified
    Log In, Sign Up button has been added

--- js/main.js
+++ js/main.js
@@ -73,6 +73,19 @@
 		autoplay : true,
 		animateOut: 'fadeOut'
 	});
+	
+	///////////////////////////
+	// Owl Carousel
+	$('#about-slider2').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animateOut: 'fadeOut'
+	});
 
 	$('#testimonial-slider').owlCarousel({
 		loop:true,
