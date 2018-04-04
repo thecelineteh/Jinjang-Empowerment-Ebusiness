acb4b2ff3bf6767077003731d888d4e07f85160d
B1601563_CT <celineteh2011@gmail.com>
2018-04-04 21:50:30 +0800

    delete old versions + updated db

--- jee.sql
+++ jee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2018 at 07:52 AM
+-- Generation Time: Apr 04, 2018 at 03:46 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -29,17 +29,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `client` (
-  `username` varchar(30) NOT NULL,
+  `userID` int(15) NOT NULL,
   `companyName` varchar(30) NOT NULL,
-  `companyDescription` varchar(30) NOT NULL
+  `companyDescription` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`username`, `companyName`, `companyDescription`) VALUES
-('celine', 'sas', '');
+INSERT INTO `client` (`userID`, `companyName`, `companyDescription`) VALUES
+(2, 'Healthcare NGO', 'Healthcare NGO is a good company');
 
 -- --------------------------------------------------------
 
@@ -48,11 +48,19 @@ INSERT INTO `client` (`username`, `companyName`, `companyDescription`) VALUES
 --
 
 CREATE TABLE `jobapplication` (
-  `applicationID` varchar(15) NOT NULL,
-  `jobID` varchar(15) NOT NULL,
-  `applicant` varchar(30) NOT NULL,
+  `applicationID` int(15) NOT NULL,
+  `jobID` int(15) NOT NULL,
+  `theJobSeeker` int(15) NOT NULL,
   `status` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jobapplication`
+--
+
+INSERT INTO `jobapplication` (`applicationID`, `jobID`, `theJobSeeker`, `status`) VALUES
+(1, 3, 1, 'ACCEPTED'),
+(2, 1, 3, 'PENDING');
 
 -- --------------------------------------------------------
 
@@ -61,24 +69,30 @@ CREATE TABLE `jobapplication` (
 --
 
 CREATE TABLE `jobposition` (
-  `jobID` varchar(15) NOT NULL,
+  `jobID` int(15) NOT NULL,
   `title` varchar(30) NOT NULL,
   `description` varchar(255) NOT NULL,
   `salaryPerHour` double(5,2) NOT NULL,
-  `hoursPerWeek` int(3) NOT NULL,
-  `durationInWeeks` int(3) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `startTime` time NOT NULL,
+  `endTime` time NOT NULL,
   `address` varchar(255) NOT NULL,
   `city` varchar(30) NOT NULL,
   `status` varchar(15) NOT NULL,
-  `theClient` varchar(30) NOT NULL
+  `theClient` int(15) NOT NULL,
+  `theEmployee` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `jobposition`
 --
 
-INSERT INTO `jobposition` (`jobID`, `title`, `description`, `salaryPerHour`, `hoursPerWeek`, `durationInWeeks`, `address`, `city`, `status`, `theClient`) VALUES
-('J1', 'job position', 'job position for weaving', 1.00, 3, 6, '1212 Jln Beta', 'KL', 'AVAILABLE', 'celine');
+INSERT INTO `jobposition` (`jobID`, `title`, `description`, `salaryPerHour`, `startDate`, `endDate`, `startTime`, `endTime`, `address`, `city`, `status`, `theClient`, `theEmployee`) VALUES
+(1, 'Test', 'Currently seeking a part time knitter for charity drive event.', 1.50, '2018-04-03', '2018-04-10', '12:00:00', '15:00:00', '21B, Jalan Cantik, 78430, Kuala Lumpur', 'Kuala Lumpur', 'AVAILABLE', 2, 0),
+(2, 'UX designer', 'Design modern UX interface for our company\'s financial department', 2.00, '2018-03-23', '2018-03-30', '17:00:00', '20:00:00', '56 Bubbly Road', 'At Home', 'NOT AVAILABLE', 2, 1),
+(3, 'Babysitter', 'Babysitter for kindergarten during a day trip. For more info, please send a message.', 2.00, '2018-04-20', '2018-05-01', '08:00:00', '15:00:00', '12 Street, Jalan 9, Sekolah Kinder', 'Kuala Lumpur', 'AVAILABLE', 2, 0),
+(4, 'Weaving', 'Weaving fabrics', 1.00, '2018-04-25', '2018-04-30', '10:00:00', '15:30:00', '239 Street', 'Kuala Lumpur', 'AVAILABLE', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -87,9 +101,24 @@ INSERT INTO `jobposition` (`jobID`, `title`, `description`, `salaryPerHour`, `ho
 --
 
 CREATE TABLE `jobrequiredskill` (
-  `jobID` varchar(15) NOT NULL,
-  `skillID` varchar(15) NOT NULL
+  `jobID` int(15) NOT NULL,
+  `skillID` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jobrequiredskill`
+--
+
+INSERT INTO `jobrequiredskill` (`jobID`, `skillID`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(2, 3),
+(2, 4),
+(3, 5),
+(4, 1);
 
 -- --------------------------------------------------------
 
@@ -98,7 +127,7 @@ CREATE TABLE `jobrequiredskill` (
 --
 
 CREATE TABLE `jobseeker` (
-  `username` varchar(30) NOT NULL,
+  `userID` int(15) NOT NULL,
   `fullName` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -106,9 +135,9 @@ CREATE TABLE `jobseeker` (
 -- Dumping data for table `jobseeker`
 --
 
-INSERT INTO `jobseeker` (`username`, `fullName`) VALUES
-('admin', 'Admin Name'),
-('admin2', 'Admin2');
+INSERT INTO `jobseeker` (`userID`, `fullName`) VALUES
+(1, 'Leah A/P Pradeep'),
+(3, 'admin');
 
 -- --------------------------------------------------------
 
@@ -117,11 +146,12 @@ INSERT INTO `jobseeker` (`username`, `fullName`) VALUES
 --
 
 CREATE TABLE `message` (
-  `messageID` varchar(15) NOT NULL,
+  `messageID` int(15) NOT NULL,
+  `subject` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `sender` varchar(30) NOT NULL,
-  `receiver` varchar(30) NOT NULL
+  `sender` int(15) NOT NULL,
+  `receiver` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -131,7 +161,7 @@ CREATE TABLE `message` (
 --
 
 CREATE TABLE `skill` (
-  `skillID` varchar(15) NOT NULL,
+  `skillID` int(15) NOT NULL,
   `skillName` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -140,29 +170,11 @@ CREATE TABLE `skill` (
 --
 
 INSERT INTO `skill` (`skillID`, `skillName`) VALUES
-('SK1', 'weaving'),
-('SK2', 'knitting'),
-('SK3', 'baking');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `skillset`
---
-
-CREATE TABLE `skillset` (
-  `username` varchar(30) NOT NULL,
-  `skillID` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `skillset`
---
-
-INSERT INTO `skillset` (`username`, `skillID`) VALUES
-('admin', 'SK2'),
-('admin', 'SK3'),
-('celine', 'SK2');
+(1, 'weaving'),
+(2, 'knitting'),
+(3, 'baking'),
+(4, 'cooking'),
+(5, 'babysitting');
 
 -- --------------------------------------------------------
 
@@ -171,9 +183,10 @@ INSERT INTO `skillset` (`username`, `skillID`) VALUES
 --
 
 CREATE TABLE `user` (
+  `userID` int(15) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `address` varchar(30) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `phoneNo` varchar(15) NOT NULL,
   `email` varchar(30) NOT NULL,
   `userType` varchar(15) NOT NULL
@@ -183,11 +196,10 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `address`, `phoneNo`, `email`, `userType`) VALUES
-('admin', 'admin', '123 Street', '123', '123@hotmail.com', 'Job Seeker'),
-('admin2', 'admin2', 'Admin Address Street 11', '019999999', 'admin2@gmail.com', 'Job Seeker'),
-('adrian', 'adrian', '8 Road', '123', '123', 'Client'),
-('celine', 'celine', 'Jalan Merah', '0192823473', 'celine@gmail.com', 'Client');
+INSERT INTO `user` (`userID`, `username`, `password`, `address`, `phoneNo`, `email`, `userType`) VALUES
+(1, 'leah', 'leah123', '123 Street, Kuala Lumpur', '0118348199', 'leahemail@hotmail.com', 'Job Seeker'),
+(2, 'ngo', 'ngo123', '9 Jalan Istimewa, NGO Berhad', '0129384654', 'ngo@gmail.com', 'Client'),
+(3, 'admin', 'admin', '', '123', 'aasdf@hotmai.com', 'Job Seeker');
 
 --
 -- Indexes for dumped tables
@@ -197,43 +209,44 @@ INSERT INTO `user` (`username`, `password`, `address`, `phoneNo`, `email`, `user
 -- Indexes for table `client`
 --
 ALTER TABLE `client`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`userID`);
 
 --
 -- Indexes for table `jobapplication`
 --
 ALTER TABLE `jobapplication`
-  ADD PRIMARY KEY (`applicationID`,`jobID`,`applicant`),
-  ADD KEY `fk_jobapplication_jobID` (`jobID`),
-  ADD KEY `fk_jobapplication_applicant` (`applicant`);
+  ADD PRIMARY KEY (`applicationID`),
+  ADD KEY `jobID` (`jobID`),
+  ADD KEY `userID` (`theJobSeeker`);
 
 --
 -- Indexes for table `jobposition`
 --
 ALTER TABLE `jobposition`
-  ADD PRIMARY KEY (`jobID`,`theClient`),
-  ADD KEY `fk_jobpos_theClient` (`theClient`);
+  ADD PRIMARY KEY (`jobID`),
+  ADD KEY `theClient` (`theClient`),
+  ADD KEY `theEmployee` (`theEmployee`);
 
 --
 -- Indexes for table `jobrequiredskill`
 --
 ALTER TABLE `jobrequiredskill`
   ADD PRIMARY KEY (`jobID`,`skillID`),
-  ADD KEY `fk_reqskill_skillID` (`skillID`);
+  ADD KEY `skillID` (`skillID`);
 
 --
 -- Indexes for table `jobseeker`
 --
 ALTER TABLE `jobseeker`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`userID`);
 
 --
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
   ADD PRIMARY KEY (`messageID`),
-  ADD KEY `FK1` (`receiver`),
-  ADD KEY `FK2` (`sender`);
+  ADD KEY `receiver` (`receiver`),
+  ADD KEY `sender` (`sender`);
 
 --
 -- Indexes for table `skill`
@@ -242,17 +255,45 @@ ALTER TABLE `skill`
   ADD PRIMARY KEY (`skillID`);
 
 --
--- Indexes for table `skillset`
---
-ALTER TABLE `skillset`
-  ADD PRIMARY KEY (`username`,`skillID`),
-  ADD KEY `fk_skillset_skillID` (`skillID`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jobapplication`
+--
+ALTER TABLE `jobapplication`
+  MODIFY `applicationID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `jobposition`
+--
+ALTER TABLE `jobposition`
+  MODIFY `jobID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `messageID` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `skill`
+--
+ALTER TABLE `skill`
+  MODIFY `skillID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -262,47 +303,40 @@ ALTER TABLE `user`
 -- Constraints for table `client`
 --
 ALTER TABLE `client`
-  ADD CONSTRAINT `fk_client_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `client_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `jobapplication`
 --
 ALTER TABLE `jobapplication`
-  ADD CONSTRAINT `fk_jobapplication_applicant` FOREIGN KEY (`applicant`) REFERENCES `user` (`username`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_jobapplication_jobID` FOREIGN KEY (`jobID`) REFERENCES `jobposition` (`jobID`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `jobapplication_ibfk_1` FOREIGN KEY (`jobID`) REFERENCES `jobposition` (`jobID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `jobapplication_ibfk_2` FOREIGN KEY (`theJobSeeker`) REFERENCES `user` (`userID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `jobposition`
 --
 ALTER TABLE `jobposition`
-  ADD CONSTRAINT `fk_jobpos_theClient` FOREIGN KEY (`theClient`) REFERENCES `user` (`username`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `jobposition_ibfk_1` FOREIGN KEY (`theClient`) REFERENCES `user` (`userID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `jobrequiredskill`
 --
 ALTER TABLE `jobrequiredskill`
-  ADD CONSTRAINT `fk_reqskill_jobID` FOREIGN KEY (`jobID`) REFERENCES `jobposition` (`jobID`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_reqskill_skillID` FOREIGN KEY (`skillID`) REFERENCES `skill` (`skillID`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `jobrequiredskill_ibfk_1` FOREIGN KEY (`jobID`) REFERENCES `jobposition` (`jobID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `jobrequiredskill_ibfk_2` FOREIGN KEY (`skillID`) REFERENCES `skill` (`skillID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `jobseeker`
 --
 ALTER TABLE `jobseeker`
-  ADD CONSTRAINT `fk_jobseeker_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `jobseeker_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `message`
 --
 ALTER TABLE `message`
-  ADD CONSTRAINT `fk_message_receiver` FOREIGN KEY (`receiver`) REFERENCES `user` (`username`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_message_sender` FOREIGN KEY (`sender`) REFERENCES `user` (`username`) ON UPDATE CASCADE;
-
---
--- Constraints for table `skillset`
---
-ALTER TABLE `skillset`
-  ADD CONSTRAINT `fk_skillset_skillID` FOREIGN KEY (`skillID`) REFERENCES `skill` (`skillID`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_skillset_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`receiver`) REFERENCES `user` (`userID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`sender`) REFERENCES `user` (`userID`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
